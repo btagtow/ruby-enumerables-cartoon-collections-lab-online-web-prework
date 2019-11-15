504cedc2c5219ab1d3e0dd70691daf6b8c997d59
Ben Tagtow <bentagtow@gmail.com>
2019-11-15 16:52:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def long_planeteer_calls(idk)
 end
 
 def find_the_cheese(array)
-  array.find { |index| index == "cheddar" || index == 
+  array.find { |index| index == "cheddar" || index == "gouda" || index == "camembert" }
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
