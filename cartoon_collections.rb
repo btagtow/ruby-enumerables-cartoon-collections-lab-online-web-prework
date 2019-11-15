ef5c1411656be8cf434827872227c15a9748adca
Ben Tagtow <bentagtow@gmail.com>
2019-11-15 16:27:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(array)
-  array=array.each.with_index { |dwarf,index| 
+  array.each_with_index { |dwarf,index| 
   puts "#{index+1}. #{dwarf}"}
 end 
   #a=[11,22,31,224,44].each_with_index { |val,index| puts "index: #{index} for #{val}" if val < 30}
