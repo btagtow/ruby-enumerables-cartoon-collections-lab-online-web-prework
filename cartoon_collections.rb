6f9573e1a5fb2d2462013dbf1cb193ec66b0801f
Ben Tagtow <bentagtow@gmail.com>
2019-11-15 16:51:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,8 @@ def long_planeteer_calls(idk)
   idk.any? { |array| array.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
+  
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
