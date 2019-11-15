896d8a61e27c8c10d49cac59f25a9efcad96e7c9
Ben Tagtow <bentagtow@gmail.com>
2019-11-15 16:33:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def roll_call_dwarves(array)
 end 
 
 def summon_captain_planet(planeteer_calls) 
-  planeteer_calls.collect do |array|
+  planeteer_calls.collect do { |array|
     puts array.capitilize
   end 
   # Your code here
